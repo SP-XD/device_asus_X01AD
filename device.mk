f5d88a97da10b74ccf14f19e560b89621a18b850
SP-XD <somnatypaul007@gmail.com>
2021-03-13 23:50:07 +0530

    fixed some names

--- device.mk
+++ device.mk
@@ -13,11 +13,11 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := X01A,X01AD
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=288
+    ro.sf.lcd_density=420
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
